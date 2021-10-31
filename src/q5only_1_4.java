24cf638ec0b6f51e7d3e4aac6b184ad5d2a55de4
Himanshu Sharma <hsallrounder@gmail.com>
2021-10-31 16:05:42 +0530

    2nd commit

--- src/q5only_1_4.java
+++ src/q5only_1_4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class q5only14 {
+public class q5only_1_4 {
     /*
     only14([1, 4, 1, 4]) → true
     only14([1, 4, 2, 4]) → false
@@ -22,7 +22,7 @@ public class q5only14 {
         for(int i=0 ; i<arr.length ; i++){
             arr[i]= sc.nextInt();
         }
-        q5only14 obj = new q5only14();
+        q5only_1_4 obj = new q5only_1_4();
         System.out.println(obj.arrcheck(arr));
     }
 }
